5df39c19350b3d303c27ade3375872c893f61fad
brunau1 <brunosantosprotec@gmail.com>
2019-08-21 15:38:35 -0300

    created vertical colision

--- source/game.js
+++ source/game.js
@@ -22,8 +22,12 @@ window.onload = () => {
 
 function moveBall() {
     ballPos.x = ballPos.x + ballSpeed.x
-    if(ballPos.x == 800 - ballSize.width) ballSpeed.x = -5
-    else if(ballPos.x == 0) ballSpeed.x = 5
+    if(ballPos.x >= canvas.width - ballSize.width) ballSpeed.x = -ballSpeed.x
+    else if(ballPos.x <= 0) ballSpeed.x = -ballSpeed.x
+
+    ballPos.y = ballPos.y + ballSpeed.y
+    if(ballPos.y >= canvas.height - ballSize.height) ballSpeed.y = -ballSpeed.y
+    else if(ballPos.y <= 0) ballSpeed.y = -ballSpeed.y
 }
 
 function draw() {
@@ -31,7 +35,7 @@ function draw() {
     context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = 'white';
-    context.fillRect(0, 200, palletSize.width, palletSize.height);
+    context.fillRect(0, canvas.height/2 - palletSize.height/2 , palletSize.width, palletSize.height);
     context.fillStyle = 'red';
     context.fillRect(ballPos.x, ballPos.y, ballSize.width, ballSize.height);
 }
