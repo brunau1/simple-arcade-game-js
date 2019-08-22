a60b49e7b69d1eabf932a3bbae56837b17b61c9a
brunau1 <brunosantosprotec@gmail.com>
2019-08-22 11:45:10 -0300

    creating generic function for canvas colision

--- source/game.js
+++ source/game.js
@@ -2,9 +2,11 @@ const { canvas, context } = getCanvasAndContext()
 
 var ballPos = { x: 400, y: 300 }
 var ballSpeed = { x: 5, y: 5 }
-var ballSize = { width: 10, height: 10}
+var ballSize = { width: 10, height: 10 }
 
-var palletSize = { width: 15, height: 100}
+var palletSize = { width: 15, height: 100 }
+var palletPos = { x: 0, y: 0 }
+var palletSpeed = { x: 0, y: 8 }
 
 function getCanvasAndContext() {
     const canvas = document.querySelector('canvas');
@@ -15,27 +17,47 @@ function getCanvasAndContext() {
 window.onload = () => {
     var frameRate = 60
     setInterval(() => {
+        console.clear()
         moveBall()
+        movePallet()
         draw()
-    }, 1000/frameRate);
+    }, 1000 / frameRate);
 }
 
 function moveBall() {
-    ballPos.x = ballPos.x + ballSpeed.x
-    if(ballPos.x >= canvas.width - ballSize.width) ballSpeed.x = -ballSpeed.x
-    else if(ballPos.x <= 0) ballSpeed.x = -ballSpeed.x
+    console.log(`Ball PosX: ${ballPos.x}`)
+    console.log(`Ball PosY: ${ballPos.y}`)
 
+    ballPos.x = ballPos.x + ballSpeed.x
     ballPos.y = ballPos.y + ballSpeed.y
-    if(ballPos.y >= canvas.height - ballSize.height) ballSpeed.y = -ballSpeed.y
-    else if(ballPos.y <= 0) ballSpeed.y = -ballSpeed.y
+
+    canvasColision(ballPos, ballSpeed, ballSize)
+}
+
+function movePallet() {
+    console.log(`Pallet PosY: ${palletPos.y}`)
+
+    palletPos.y = palletPos.y + palletSpeed.y
+
+    canvasColision(palletPos, palletSpeed, palletSize)
+}
+
+function canvasColision(pos, speed, size) {
+    if (pos.y >= canvas.height - size.height) speed.yy = -speed.y
+    else if (pos.y <= 0) speed.y = -speed.y
+
+    if (pos.x >= canvas.width - size.width) speed.x = -speed.x
+    else if (pos.x <= 0) speed.x = -speed.x
 }
 
 function draw() {
     console.log('drawing')
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = 'white';
-    context.fillRect(0, canvas.height/2 - palletSize.height/2 , palletSize.width, palletSize.height);
-    context.fillStyle = 'red';
-    context.fillRect(ballPos.x, ballPos.y, ballSize.width, ballSize.height);
+    createRect(0, 0, canvas.width, canvas.height, 'black');
+    createRect(palletPos.x, palletPos.y, palletSize.width, palletSize.height, 'white');
+    createRect(ballPos.x, ballPos.y, ballSize.width, ballSize.height, 'red');
+}
+
+function createRect(posX, posY, width, height, color) {
+    context.fillStyle = color;
+    context.fillRect(posX, posY, width, height);
 }
